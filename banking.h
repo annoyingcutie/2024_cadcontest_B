fbe719688c5ae5997b21fbc058c667ff4a8397bb
MeganTsou <yunhwa.tsou@gmail.com>
2024-06-12 21:52:55 +0800

    message

--- banking.h
+++ banking.h
@@ -14,20 +14,29 @@ public:
         for (int i=0; i<circuit.FF_instances.size(); i++){
             // insert all flipflops to ctree
             circuit.FF_instances[i].id = i;
-            for (int j=0; j<circuit.flipflops.size(); j++){
-                if ( circuit.FF_instances[i].type == circuit.flipflops[j].name ){
-                    circuit.FF_instances[i].width = circuit.flipflops[j].x;
-                    circuit.FF_instances[i].height = circuit.flipflops[j].y;
+            for (int j=0; j<circuit.flipFlops.size(); j++){
+                if ( circuit.FF_instances[i].type == circuit.flipFlops[j].name ){
+                    circuit.FF_instances[i].width = circuit.flipFlops[j].x;
+                    circuit.FF_instances[i].height = circuit.flipFlops[j].y;
                     ctree.Insert( {circuit.FF_instances[i].x, circuit.FF_instances[i].y}, \
-                    {circuit.FF_instances[i].x + circuit.flipflops[j].x, circuit.FF_instances[i].y + circuit.flipflops[j].y}, i);
+                    {circuit.FF_instances[i].x + circuit.flipFlops[j].x, circuit.FF_instances[i].y + circuit.flipFlops[j].y}, i);
                 }
                 break;
             }
         }
 
-        // Find maximum bits of flipflop
-        for (int i=0; i<circuit.flipflops.size(); i++){
-            if (circuit.flipflops[i].id > max_bit) max_bit = circuit.flipflops[i].id;
+        // // Find maximum bits of flipflop
+        // for (int i=0; i<circuit.flipFlops.size(); i++){
+        //     if (circuit.flipFlops[i].id > max_bit) max_bit = circuit.flipFlops[i].id;
+        // }
+        for (int i=0; i<circuit.flipFlops.size(); i++){
+            bits.push(circuit.flipFlops[i].id);
+        }
+
+        for (int i=0; i<bits.size(); i++){
+            for (int j=0; j<bits.size){
+
+            }
         }
 
 
@@ -36,10 +45,10 @@ public:
 
     
     // Find all neightbors of all flipflops
-    std::vector<int> findAdjacentFF(Instance flipflop, float epsilon = 2000) {
+    std::vector<int> findAdjacentFF(Instance flipflop, float epsilon = 2000) {/////alter
         // neighbors.clear();
         neighbors_id.clear();
-        count_neighbor = 0;
+        // count_neighbor = 0;
         float query_MinBounds[2] = {flipflop.x - epsilon, flipflop.y - epsilon}
         float query_MaxBounds[2] = {flipflop.x + flipflop.width + epsilon, flipflop.y + flipflop.height + epsilon};
         ctree.Search(query_MinBounds[2], query_MaxBounds[2],  [](Instance const& target){
@@ -59,18 +68,21 @@ public:
             std::vector<int> neighbors_id_return;
             neighbors_id_return = findAdjacentFF(ff_list[index]);
             //
-            for (int i=0; i<neighbors_id_return.size(); i++){
-                std::vector<Instance>::iterator itor = ff_list.begin();
-                while (itor != ff_list.end()){
-                    if (*itor.id == neighbors_id_return[i]){
-                        itor = ff_list.erase(itor);
-                        break;
-                    } 
-                    else itor++;
+            if ( !neighbors_id_return.empty() ){
+                for (int i=0; i<neighbors_id_return.size(); i++){
+                    std::vector<Instance>::iterator itor = ff_list.begin();
+                    while (itor != ff_list.end()){
+                        if (*itor.id == neighbors_id_return[i]){
+                            itor = ff_list.erase(itor);
+                            break;
+                        } 
+                        else itor++;
+                    }
                 }
             }
             ff_list.erase(ff_list.begin()+index);
 
+            resultFF 
 
 
             index++;
@@ -83,15 +95,19 @@ public:
 
 private:
     // Circuit circuit;
-    int count_neighbor;
-    int max_bit;
+    // int count_neighbor;
+    // int max_bit;
     // std::vector<Instance> neighbors;
+    std::vector<int> bits;
     std::vector<int> neighbors_id;
 
     static bool MySearchCallback(Instance& target) {
-        count_neighbor++;
+        // count_neighbor++;
         neighbors_id.push(target.id);
-        if (count_neighbor >= max_bit) return false;
-        else return true;
+
+        return true;
+        // if (count_neighbor >= max_bit) return false;
+        // else return true;
+
     }
 };
